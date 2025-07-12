fc62b2b83019899b2b36d631d0fba60787588738
Adnan <adnanzafar2001@gmail.com>
2025-07-13 01:20:33 +1000

    default layout updated for the project,font size and images added

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/avatar.jpg'),
+              ),
+              Text(
+                'Adnan Zafar',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40.0,
+                ),
+
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+
+
